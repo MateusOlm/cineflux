d2cd5702e22bad5863a2a39b984ccf123e7e70dc
Mateus <mateuslimado1234@gmail.com>
2026-03-16 16:38:56 -0300

    backend: Criação do projeto cineflux

---
+++ src/main/java/mateus/cineflux/CinefluxApplication.java
@@ -0,0 +1,15 @@
+package mateus.cineflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CinefluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefluxApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/mateus/cineflux/CinefluxApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus.cineflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
