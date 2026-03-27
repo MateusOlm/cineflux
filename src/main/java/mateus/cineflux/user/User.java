eb1c939bfdcb7392aa6e2cb57553fff6d92cf278
Mateus <mateuslimado1234@gmail.com>
2026-03-27 14:05:02 -0300

    (feat): Criando usuario base da aplicação

---
+++ src/main/java/mateus/cineflux/user/User.java
@@ -0,0 +1,39 @@
+package mateus.cineflux.user;
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public UUID getUserId() {
+        return this.userId;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
